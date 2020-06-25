517ac6be66116786fd59b869a10974c7f615dc0a
SamaraGarrett <samaraegarrett@gmail.com>
2020-06-25 16:38:47 -0500

    Wording changes

--- projects.php
+++ projects.php
@@ -6,15 +6,24 @@ session_start();
 ?>
 
 <div class="container">
+    <h1>This Website</h1>
+    <p>PHP <!-- and MySQL three-tier (in development)-->web application.</p>
+    <ul>
+        <li>Designed and implemented web application.</li>
+    </ul>
+    <p>Link to github for project: <a href="https://github.com/SamaraGarrett/samaragarrett">Samara Garrett</a>.</p>
+</div>
+
+<div class="container">
     <h1>Tool Rental System</h1>
     <p>ASP.NET Core and MySQL three-tier web application.</p>
-    <p>Metropolitan State University Bachelor of Science in Computer Science Capstone Project.
+    <p>Capstone Project for Metropolitan State University Bachelor of Science in Computer Science.
     Built application from the ground up with a group of four other students.</p>
     <ul>
+        <li>Collaborated with team on design of website look, structure, and database.</li>
         <li>Designed the Use Case and Class Diagrams for the project.</li>
         <li>Managed team, inluding assigning tasks and monitoring progress.</li>
         <li>Recorded minutes for twice weekly meeting and compiled them online to be referenced by group.</li>
-        <li>Collaborated on database design.</li>
         <li>Implemented several use cases, including Rent Out Tool, Return Tool, and display and search of tables (using jQuery DataTables).</li>
     </ul>
     <p>Link to github for project: <a href="https://github.com/lancefox1979/ToolRentalSystem.Web">Tool Registration System</a>. My commits are under the GitHub account SamaraEGarrett.</p>
@@ -36,22 +45,24 @@ session_start();
         <li>Discovered and fixed a bug that had caused important records to be deleted.</li>
         <li>Other bug fixes:
             <ul>
-                <li>display of registered classes and balance due</li>
-                <li>adjustments to display of data so that it fit logic requirements given by supervisor</li> 
-                <li>fixed confirmation email behavior for registration</li>
+                <li>Corrected display of registered classes and balance due.</li>
+                <li>Adjustments to display of data so that it fit logic requirements given by supervisor.</li> 
+                <li>Fixed confirmation email behavior for registration.</li>
             </ul>
         </li>
         <li>Enhancements to existing pages:
             <ul>
-                <li>auto-sort data by last name</li>
-                <li>added visual indicators for paid/unpaid buttons</li>
-                <li>created and formatted a printable report of data</li>
-                <li>added detail to summaries of various pages</li>
-                <li>added recover password functionality</li>
+                <li>Auto-sort data by last name.</li>
+                <li>Added visual indicators for paid/unpaid buttons.</li>
+                <li>Created and formatted a printable report of data.</li>
+                <li>Added detail to summaries of various pages.</li>
+                <li>Added recover password functionality.</li>
             </ul>
         </li>
     </ul>
 </div>
+<br>
+<br>
 
 <?php
 require('footer.php');
---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php
+DEFINE('PAGE_TITLE','Login');
+require('header.php');
+?>
+
+<div class="container">
+    <h1>Under Construction</h1>
+</div>
+
+<?php
+require('footer.php');
+?>
